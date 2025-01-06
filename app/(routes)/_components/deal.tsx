e3ecd7381287da91779bba286ed70920d6b7332d
kaye2512 <kayeromy@gmail.com>
2025-01-06 13:46:43 +0100

    Enhance Deal component and update VogueLogo styling
    - Added Deal component to showcase promotional items with images and titles for better visibility.
    - Introduced new images for the Deal component: 'Rectangle 56.png' and 'console.png'.
    - Updated VogueLogo component to adjust image width for improved responsiveness.

--- app/(routes)/_components/deal.tsx
+++ app/(routes)/_components/deal.tsx
@@ -1,12 +1,40 @@
 import Container from "@/components/container";
+import Image from "next/image";
+import rectangle from "@/public/images/Rectangle 56.png";
+import console from "@/public/images/console.png";
 
 export default function Deal() {
   return (
-    <Container className="flex flex-col gap-8 md:flex-row md:gap-8 md:mt-8 lg:mt-12 lg:gap-32 lg:flex-row">
-      <div className="w-32 h-32 md:w-64 md:h-64 lg:w-96 lg:h-96 bg-red-500">
-        <div></div>
+    <Container className="flex flex-col gap-8 mt-4 md:flex-row md:gap-8 md:mt-8 lg:mt-12 lg:gap-32 lg:flex-row">
+      <div className="flex w-auto h-64 md:w-64 md:h-64 lg:w-96 lg:h-96 bg-[#F3F4F6] justify-between items-center gap-6 px-12">
+        <div>
+          <h1 className="font-bold text-2xl">Best Deal</h1>
+        </div>
+        <div className="">
+          <Image
+            src={rectangle}
+            alt="Deal"
+            width={100}
+            height={100}
+            className="w-40 h-24"
+          />
+        </div>
       </div>
-      <div className="w-32 h-32 md:w-64 md:h-64 lg:w-96 lg:h-96 bg-blue-500"></div>
+
+      <div className="flex w-auto h-64 md:w-64 md:h-64 lg:w-96 lg:h-96 bg-[#F3F4F6] justify-between items-center gap-6 px-12">
+        <div>
+          <h1 className="font-bold text-2xl">Game Console</h1>
+        </div>
+        <div className="">
+          <Image
+            src={console}
+            alt="Deal"
+            width={100}
+            height={100}
+            className="w-40 h-24"
+          />
+        </div>
+      </div>
     </Container>
   );
 }
--- app/(routes)/_components/vogue-logo.tsx
+++ app/(routes)/_components/vogue-logo.tsx
@@ -5,7 +5,7 @@ export default function VogueLogo() {
   return (
     <Image
       src={vogue}
-      width={200}
+      width={100}
       height={100}
       alt="vogue-logo"
       className="h-5 w-40 md:h-4 md:w-32 lg:h-6 lg:w-32"
