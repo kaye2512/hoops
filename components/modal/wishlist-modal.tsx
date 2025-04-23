6484235297dd6e60159386929666d0ed9e1990ac
kaye2512 <kayeromy@gmail.com>
2025-04-23 20:17:49 +0200

    Enhance WishlistModal with product details and action buttons
    - Added product information including SKU, color, size, and price to the WishlistModal.
    - Integrated action buttons for "More information" and "Add to cart" to improve user interaction.
    - Improved layout structure for better visual presentation of wishlist items.

--- components/modal/wishlist-modal.tsx
+++ components/modal/wishlist-modal.tsx
@@ -1,6 +1,7 @@
 import useWishlistModal from "@/hooks/use-wishlist-modal";
 import { Modal } from "../ui/modal";
 import WishlistCard from "../wishlist-card";
+import { Button } from "../ui/button";
 
 export default function WishlistModal() {
   const WishlistModal = useWishlistModal();
@@ -15,6 +16,21 @@ export default function WishlistModal() {
         <WishlistCard />
         <WishlistCard />
         <WishlistCard />
+      </div>
+      <div className="flex flex-col gap-2 mt-4">
+        <p className="text-xs font-medium text-[#6B7280]">MK617</p>
+        <p className="text-xs font-medium text-[#1D1D1F]">Belge brown</p>
+        <div className="flex flex-col gap-2">
+          <p className="text-xs  text-[#1D1D1F]">42 sizes</p>
+
+          <p className="text-xs  text-[#1D1D1F]">$1,000</p>
+        </div>
+      </div>
+      <div className="flex  gap-4 mt-4">
+        <Button variant={"outline"} className="rounded-none">
+          More information
+        </Button>
+        <Button className="bg-[#111827] rounded-none w-40">Add to cart</Button>
       </div>
     </Modal>
   );
